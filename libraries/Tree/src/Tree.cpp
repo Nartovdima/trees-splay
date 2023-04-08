5ce8aaf0ca511e6ad7d5d917dab005f736eaf58b
Nartov Dima <nartov.dima2018@yandex.ru>
2023-04-08 11:09:38 +0300

    Let's run tests

--- libraries/Tree/src/Tree.cpp
+++ libraries/Tree/src/Tree.cpp
@@ -1,163 +1,158 @@
 #include "tree/Tree.hpp"
 
-void Splay::rotate_left(Splay::Node *node) const noexcept {
-    Node* father = node -> parent;
-    Node* a = node -> right;
-    Node* b = a -> left;
+void Splay::rotate_left(Splay::Node* node) const noexcept {
+    Node* father = node->parent;
+    Node* a      = node->right;
+    Node* b      = a->left;
 
     if (father) {
-        if (father -> left == node) {
-            father -> left = a;
+        if (father->left == node) {
+            father->left = a;
         } else {
-            father -> right = a;
+            father->right = a;
         }
     }
 
     if (b) {
-        b -> parent = node;
+        b->parent = node;
     }
 
-    a -> parent = father;
-    a -> left = node;
-    node -> parent = a;
-    node -> right = b;
+    a->parent    = father;
+    a->left      = node;
+    node->parent = a;
+    node->right  = b;
 }
 
-void Splay::rotate_right(Splay::Node *node) const noexcept {
-   Node* father = node -> parent;
-   Node* a = node -> left;
-   Node* b = a-> right;
+void Splay::rotate_right(Splay::Node* node) const noexcept {
+    Node* father = node->parent;
+    Node* a      = node->left;
+    Node* b      = a->right;
 
-   if (father) {
-       if (father -> left == node) {
-           father -> left = a;
-       } else {
-            father -> right = a;
-       }
-   }
+    if (father) {
+        if (father->left == node) {
+            father->left = a;
+        } else {
+            father->right = a;
+        }
+    }
 
-   if (b) {
-       b -> parent = node;
-   }
+    if (b) {
+        b->parent = node;
+    }
 
-   a -> parent = father;
-   a -> right = node;
-   node -> parent = a;
-   node -> left = b;
+    a->parent    = father;
+    a->right     = node;
+    node->parent = a;
+    node->left   = b;
 }
 
-bool Splay::zig(Splay::Node *x) const noexcept {
-   Node* father = x -> parent;
-   if (father -> left == x) {
-       rotate_right(father);
-   } else {
-       rotate_left(father);
-   }
-   return true;
+bool Splay::zig(Splay::Node* x) const noexcept {
+    Node* father = x->parent;
+    if (father->left == x) {
+        rotate_right(father);
+    } else {
+        rotate_left(father);
+    }
+    return true;
 }
 
 bool Splay::zig_zag(Splay::Node* x) const noexcept {
-   Node* father = x -> parent;
-   Node* grand_father = father -> parent;
-   bool change = false;
-   if (grand_father-> left == father && father -> right == x) {
-       rotate_left(father);
-       rotate_right(grand_father);
-       change = true;
-   } else if (grand_father-> right == father && father -> left == x) {
-       rotate_right(father);
-       rotate_left(grand_father);
-       change = true;
-   }
-   return change;
+    Node* father       = x->parent;
+    Node* grand_father = father->parent;
+    bool change        = false;
+    if (grand_father->left == father && father->right == x) {
+        rotate_left(father);
+        rotate_right(grand_father);
+        change = true;
+    } else if (grand_father->right == father && father->left == x) {
+        rotate_right(father);
+        rotate_left(grand_father);
+        change = true;
+    }
+    return change;
 }
 
 bool Splay::zig_zig(Splay::Node* x) const noexcept {
-   Node * father = x -> parent;
-   Node * grand_father = father -> parent;
-   bool change = false;
-   if (grand_father -> left == father && father -> left == x) {
-       rotate_right(grand_father);
-       rotate_right(father);
-       change = true;
-   } else if (grand_father -> right == father && father -> right == x) {
-       rotate_left(grand_father);
-       rotate_left(father);
-       change = true;
-   }
-   return change;
+    Node* father       = x->parent;
+    Node* grand_father = father->parent;
+    bool change        = false;
+    if (grand_father->left == father && father->left == x) {
+        rotate_right(grand_father);
+        rotate_right(father);
+        change = true;
+    } else if (grand_father->right == father && father->right == x) {
+        rotate_left(grand_father);
+        rotate_left(father);
+        change = true;
+    }
+    return change;
 }
 
 void Splay::splay(Splay::Node* x) const noexcept {
-   while (x -> parent) {
-       Node* father = x -> parent;
-       Node* grand_father = father -> parent;
+    while (x->parent) {
+        Node* father       = x->parent;
+        Node* grand_father = father->parent;
 
-       if (!grand_father) {
+        if (!grand_father) {
             zig(x);
-       }
-       else {
+        } else {
             bool success = zig_zag(x);
             if (!success) {
                 zig_zig(x);
             }
-       }
-   }
-   root = x;
+        }
+    }
+    root = x;
 }
 
-
-
 Splay::Node* Splay::find(int value) const noexcept {
-   Node* current_node = root;
-   while (true) {
-       if (!current_node) {
+    Node* current_node = root;
+    while (true) {
+        if (!current_node) {
             break;
-       }
-       if (current_node -> value == value) {
+        }
+        if (current_node->value == value) {
             break;
-       } else if (current_node -> value < value) {
-            current_node = current_node -> right;
-       } else {
-            current_node = current_node -> left;
-       }
-   }
-   if (current_node) {
-       splay(current_node);
-   }
-   return current_node;
+        } else if (current_node->value < value) {
+            current_node = current_node->right;
+        } else {
+            current_node = current_node->left;
+        }
+    }
+    if (current_node) {
+        splay(current_node);
+    }
+    return current_node;
 }
 
 bool Splay::contains(int value) const noexcept {
-   return (find(value) != nullptr);
+    return (find(value) != nullptr);
 }
 
 Splay::Node* Splay::insert(Splay::Node*& node, Splay::Node*& prev_node, int value) {
     if (!node) {
-       node = new Node(value,nullptr, nullptr, prev_node);
-       return node;
+        node = new Node(value, nullptr, nullptr, prev_node);
+        return node;
     }
-    if (node -> value == value) {
-       return nullptr;
+    if (node->value == value) {
+        return nullptr;
     }
-    if (node -> value < value) {
-        insert(node -> right, node, value);
+    if (node->value < value) {
+        insert(node->right, node, value);
     } else {
-        insert(node -> left, node, value);
+        insert(node->left, node, value);
     }
 }
 
 bool Splay::insert(int value) {
     Splay::Node* abstract_node = nullptr;
-    Node* result = insert(root, abstract_node, value);
+    Node* result               = insert(root, abstract_node, value);
     if (result) {
         tree_size++;
         splay(result);
     }
     return result;
 }
-
-
 
 bool Splay::remove(int value) {
     if (!find(value)) {
@@ -166,44 +161,44 @@ bool Splay::remove(int value) {
     tree_size--;
     Node* current_node = root;
     while (true) {
-        if (current_node -> value == value) {
+        if (current_node->value == value) {
             break;
-        } else if (current_node -> value < value) {
-            current_node = current_node -> right;
+        } else if (current_node->value < value) {
+            current_node = current_node->right;
         } else {
-            current_node = current_node -> left;
+            current_node = current_node->left;
         }
     }
     splay(current_node);
 
-    Node* l = root -> left;
-    Node* r = root -> right;
+    Node* l = root->left;
+    Node* r = root->right;
 
-    root -> left = nullptr;
-    root -> right = nullptr;
+    root->left  = nullptr;
+    root->right = nullptr;
     delete root;
     root = nullptr;
 
     if (!r && l) {
-        root = l;
-        root -> parent = nullptr;
+        root         = l;
+        root->parent = nullptr;
     } else if (r && !l) {
-        root = r;
-        root -> parent = nullptr;
+        root         = r;
+        root->parent = nullptr;
     } else if (r && l) {
-        root = l;
-        root -> parent = nullptr;
+        root           = l;
+        root->parent   = nullptr;
         Node* max_node = l;
         while (true) {
-            if (max_node-> right && max_node-> right -> value > max_node-> value) {
-                max_node = max_node-> right;
+            if (max_node->right && max_node->right->value > max_node->value) {
+                max_node = max_node->right;
             } else {
                 break;
             }
         }
         splay(max_node);
-        root -> right = r;
-        r -> parent = root;
+        root->right = r;
+        r->parent   = root;
     }
     return true;
 }
@@ -216,13 +211,13 @@ bool Splay::empty() const noexcept {
     return tree_size == 0;
 }
 
-void Splay::dump_tree(Node *node, std::vector<int> &tree) const noexcept {
-    if (node -> left) {
-        dump_tree(node -> left, tree);
+void Splay::dump_tree(Node* node, std::vector<int>& tree) const noexcept {
+    if (node->left) {
+        dump_tree(node->left, tree);
     }
-    tree.push_back(node -> value);
-    if (node -> right) {
-        dump_tree(node -> right, tree);
+    tree.push_back(node->value);
+    if (node->right) {
+        dump_tree(node->right, tree);
     }
 }
 
@@ -239,5 +234,5 @@ Splay::Node::~Node() {
 }
 
 Splay::~Splay() {
-    //delete root;
+    // delete root;
 }
--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,19 @@
 #include <iostream>
-#include "Tree/Tree.hpp"
 #include <vector>
+
+#include "Tree/Tree.hpp"
 
 int main() {
     Splay a;
-    std::vector <int> b{2, 10, 5, 7, 3, 4, 1, 8, 6, 9};
-    std::vector <int> c{2, 10, 5, 7, 3};
+    std::vector<int> b{2, 10, 5, 7, 3, 4, 1, 8, 6, 9};
+    std::vector<int> c{2, 10, 5, 7, 3};
 
     for (auto &it : b) {
         a.insert(it);
     }
 
-    for(auto &it : c) {
-        std::vector <int> tmp = a.values();
+    for (auto &it : c) {
+        std::vector<int> tmp = a.values();
         for (auto &it2 : tmp) {
             std::cout << it2 << ' ';
         }
