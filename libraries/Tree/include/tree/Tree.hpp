180a8466c3a993bfc852e0aae1a62cb5db69e5bf
Nartov Dima <nartov.dima2018@yandex.ru>
2023-04-08 10:59:12 +0300

    Check tests

--- libraries/Tree/include/tree/Tree.hpp
+++ libraries/Tree/include/tree/Tree.hpp
@@ -4,8 +4,47 @@
 #include <cstddef>
 #include <cstdint>
 #include <vector>
+#include <iostream>
 
 class Splay {
+
+private:
+
+    struct Node{
+        int value;
+//        int quantity;
+        Node* left;
+        Node* right;
+        Node* parent;
+
+        Node(int value, /*int quantity,*/ Node* left, Node* right, Node* parent) {
+            this -> value = value;
+//            this -> quantity = quantity;
+            this -> left = left;
+            this -> right = right;
+            this -> parent = parent;
+        }
+
+        ~Node();
+    };
+
+    std::size_t tree_size = 0;
+    mutable Node* root = nullptr;
+
+    void rotate_left(Node* x) const noexcept;
+    void rotate_right(Node* x) const noexcept;
+
+    bool zig(Node* x) const noexcept;
+    bool zig_zig(Node* x) const noexcept;
+    bool zig_zag(Node* x) const noexcept;
+
+    void splay(Node* x) const noexcept;
+
+    void dump_tree(Node* node, std::vector<int> & tree) const noexcept;
+
+    Node* find(int value) const noexcept;
+
+    Node* insert(Node *&node, Node*& prev_node, int value);
 public:
     [[nodiscard]] bool contains(int value) const noexcept;
     bool insert(int value);
--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,23 @@
 #include <iostream>
+#include "Tree/Tree.hpp"
+#include <vector>
 
 int main() {
+    Splay a;
+    std::vector <int> b{2, 10, 5, 7, 3, 4, 1, 8, 6, 9};
+    std::vector <int> c{2, 10, 5, 7, 3};
+
+    for (auto &it : b) {
+        a.insert(it);
+    }
+
+    for(auto &it : c) {
+        std::vector <int> tmp = a.values();
+        for (auto &it2 : tmp) {
+            std::cout << it2 << ' ';
+        }
+        std::cout << std::endl;
+        a.remove(it);
+    }
     return 0;
 }
